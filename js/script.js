e6e9044b2a2aba6847200e16833b735d78eaace7
A <77360601+axsxi@users.noreply.github.com>
2025-02-25 15:54:40 +0100

    modified:   script.js

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ function loadQuestion() {
   } else {
     localStorage.setItem("answers", JSON.stringify(answers));
     localStorage.setItem("drinkCount", drinkCount);
-    window.location.href = "https://axsxi.github.io/test-purete/html/result.html";
+    window.location.href = "/test-purete/html/result.html";
   }
 }
 
